f0bd3c5c4020d6bd6c7486cf20b64fd0951e2ad1
Prince Rana <ranaprince45may@gmail.com>
2025-02-26 18:19:39 +0530

    created the register and the login controller  for the user.

--- controller/authController.js
+++ controller/authController.js
@@ -1,28 +1,50 @@
-const User = require('../model/UserModel');
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
+const User = require("../model/UserModel");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
 
-exports.registerUser = async (req,res) => {
-    try {
-        const {name,password,email} = req.body;
-        const existingUSer = await User.findOne({email});
-        if(existingUSer){
-            return res.status(400).json({message:'Email already exists'});
-        }
-
-        const hashedPassword = await bcrypt.hash(password,10);
-        const user = User.create({name,password:hashedPassword, email})
-
-        res.status(200).json({
-            message: 'User created successfully',
-            user: user
-        });
-    } catch (error) {
-            console.error(error);
-            res.status(500).json({message: 'Server error'});        
+exports.registerUser = async (req, res) => {
+  try {
+    const { name, password, email } = req.body;
+    const existingUSer = await User.findOne({ email });
+    if (existingUSer) {
+      return res.status(400).json({ message: "Email already exists" });
     }
 
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = User.create({ name, password: hashedPassword, email });
+
+    res.status(200).json({
+      message: "User created successfully",
+      user: user,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Server error" });
+  }
+};
+
+exports.userLogin = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({ message: "Invalid email or password" });
+    }
+    const isValidPassword = await bcrypt.compare(password, user.password);
+    if (!isValidPassword) {
+      return res.status(400).json({ message: "Invalid email or password" });
+    }
+    const token = jwt.sign({ userId: user._id }, process.env.SECRET_KEY, {
+      expiresIn: "1h",
+    });
+
+    res.cookie("token", token);
+    return res.json({ message: "Login successful", token });
     
-
-
-}
+  } catch (error) {
+    console.error("Error during login:", error);
+    res.status(500).json({ message: "An unexpected error occurred" });
+  }
+};
